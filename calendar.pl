46c3817335131a61de329373d2fccde94f0eddb1
nandinipatel <patelnandini26@gmail.com>
2020-12-05 00:15:42 -0500

    writing to text file

--- calendar.pl
+++ calendar.pl
@@ -6,11 +6,26 @@ day(fri).
 day(sat).
 day(sun).
 
-writeCalendar(T):- open('calendar.txt', write, Stream),
-			write(Stream, T), 
+/* 
+	writing to the text file 
+	
+	TODO: 
+		need to check for valid time
+		need to check for valid date
+*/
+writeCalendar(A, C, D, T):- open('calendar.txt', append, Stream),
+			day(A),
+			write(Stream, [A,C,D,T]), 
 			nl(Stream),
 			close(Stream).
 
+/* 
+	reading does not work 
+	
+	TODO:
+		read line by line from text file
+		it will get date as input => display all tasks of that date
+*/
 readCalendar:- open('calendar.txt', read, Stream),
 			read_file(Str, Lines),
 			close(Str),
@@ -22,4 +37,3 @@ read_file(Stream, []):- at_end_of_stream(Stream).
 read_file(Stream, [X|L]):- \+ at_end_of_stream(Stream),
 			read(Stream, X),
 			read_file(Stream, L).
-
